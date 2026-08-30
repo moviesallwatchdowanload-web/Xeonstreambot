3d069768106fb534568ac8393cb03eaf6661676d
moviesallwatchdowanload-web <moviesallwatchdowanload@gmail.com>
2026-08-30 19:40:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,3 +5,17 @@ try:
 except RuntimeError:
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
+from flask import Flask
+import threading
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "I am alive"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+t = threading.Thread(target=run)
+t.start()
